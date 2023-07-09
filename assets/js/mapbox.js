ab563a0aa0e307111dbcf2641986faabd4286788
lightoftwelve <info@lightoftwelve.com>
2023-07-08 22:59:50 -0400

    updated mapbox.js to set address to active when autofilling map, updated header.html to include hamburger menu for mobile, added mobile repsoniveness to bucketlist.html, updated style.css to reflect above changes

--- assets/js/mapbox.js
+++ assets/js/mapbox.js
@@ -1,4 +1,4 @@
-// Start of the attached the script for the map
+// Start of the attached script for the map
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3Rhbm9wMDkiLCJhIjoiY2xqcmhndWtxMGVuMzNjcnkyNjZ6eWZ5NiJ9.OSXBLFAFrFcw5S7mB93ePQ';
 var map = new mapboxgl.Map({
   container: 'map',
@@ -19,21 +19,22 @@ document.getElementById('search').appendChild(geocoder.onAdd(map));
 
 var addressInput = document.getElementById('address');
 
-geocoder.on('result', function(e) {
-  map.flyTo({center: e.result.center, zoom: 12}); // Zoom to the selected location with a zoom level of 12
+geocoder.on('result', function (e) {
+  map.flyTo({ center: e.result.center, zoom: 12 }); // Zoom to the selected location with a zoom level of 12
   var result = e.result;
   if (result.place_name) {
     addressInput.value = result.place_name;
+    $(addressInput).siblings("label").addClass("active");
   }
 });
 
-document.getElementById('search-button').addEventListener('click', function() {
+document.getElementById('search-button').addEventListener('click', function () {
   var query = document.getElementById('search').value;
   if (query !== '') {
     geocoder.query(query);
   }
 });
 
-document.getElementById('search').addEventListener('input', function() {
+document.getElementById('search').addEventListener('input', function () {
   addressInput.value = '';
 });
